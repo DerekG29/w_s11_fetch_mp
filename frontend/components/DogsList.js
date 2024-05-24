601203e29989ae63e5cb90e06b23c45a72a9a0e0
DerekG29 <derekgrubbs29@gmail.com>
2024-05-23 21:42:11 -0400

    can successfully delete a dog

--- frontend/components/DogsList.js
+++ frontend/components/DogsList.js
@@ -1,11 +1,30 @@
-import React, { useEffect} from 'react'
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function DogsList(props) {
   const { fetchDogs, dogs } = props;
+  const navigate = useNavigate();
 
   useEffect(() => {
     fetchDogs();
   }, []);
+
+  const editDog = id => {
+
+  }
+  const deleteDog = async id => {
+    try {
+      const res = await fetch(`http://localhost:3003/api/dogs/${id}`, {
+        method: 'DELETE',
+      });
+      if (!res.ok) {
+        throw new Error('There was an issue deleting the dog...')
+      }
+      fetchDogs();
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
   return (
     <div>
@@ -17,7 +36,7 @@ export default function DogsList(props) {
               {dog.name}, {dog.breed}, {dog.adopted ? 'adopted' : 'NOT adopted'}
               <div>
                 <button>Edit</button>
-                <button>Delete</button>
+                <button onClick={() => deleteDog(dog.id)}>Delete</button>
               </div>
             </li>
           )
